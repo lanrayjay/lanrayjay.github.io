edf2561903f9ec8c6fef6e10a64d2903d89e79c0
lanrayjay <54362209+lanrayjay@users.noreply.github.com>
2019-09-03 20:56:06 +0100

    Add files via upload

---
+++ cv_css/volunteer_form_validation.js
@@ -0,0 +1,70 @@
+function formValidation()
+{
+
+var fname = document.registration.fname;
+var uemail = document.registration.email;
+var umessage = document.registration.note;
+
+	if(allLetter(fname))
+	{	
+		if(ValidateEmail(uemail))
+		{
+			if(ValidateNote(umessage))
+			{
+			}
+		}
+	}
+	return false;
+} 
+
+function allLetter(fname)
+{ 
+	var letters = /^[A-Za-z\s]+$/;
+	var flen = fname.value;
+	if(fname.value.match(letters) && (flen.length > 3))
+	{
+		return true;
+	}
+	else
+	{
+		alert('Fullname must have at least four (4) characters.');
+		fname.focus();
+		return false;
+	}
+}
+
+function ValidateEmail(uemail)
+{
+	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if(uemail.value.match(mailformat))
+	{
+		return true;
+	}
+	else
+	{
+		alert("You have entered an invalid email address!");
+		uemail.focus();
+		return false;
+	}
+}
+
+function ValidateNote(umessage)
+{ 
+	var letters = /^[A-Za-z\s]+$/;
+	
+	var mesl = umessage.value; 
+	
+	if(umessage.value.match(letters) && (mesl.length > 19))
+	{
+		alert('Form Succesfully Submitted');
+		window.location.reload();
+
+		return true;
+	}
+	else
+	{
+		alert('Message must have alphabet characters only');
+		umessage.focus();
+		return false;
+	}
+}
